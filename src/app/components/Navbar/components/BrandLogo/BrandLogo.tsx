7e99c869fe2ba29314ab736e6917456ace35052c
Elgun Mehdi <elgun88@gmail.com>
2024-05-21 09:34:22 -0600

    feat: create BrandLogo ui component for navbar

---
+++ src/app/components/Navbar/components/BrandLogo/BrandLogo.tsx
@@ -0,0 +1,12 @@
+import { BoxIcon } from 'lucide-react';
+
+function BrandLogo() {
+  return (
+    <div className='flex items-center space-x-2'>
+      <BoxIcon strokeWidth={2.2} />
+      <p className='text-lg font-semibold'>ImageBox</p>
+    </div>
+  );
+}
+
+export default BrandLogo;
---
+++ src/app/components/Navbar/components/index.tsx
@@ -0,0 +1,3 @@
+import BrandLogo from './BrandLogo/BrandLogo';
+
+export { BrandLogo };
