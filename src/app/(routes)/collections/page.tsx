e7ef9467046a4565bae9ec58f20fed8c05366ec6
Elgun Mehdi <elgun88@gmail.com>
2024-06-02 09:35:53 -0600

    feat: manage user auth status in collections page

--- src/app/(routes)/collections/page.tsx
+++ src/app/(routes)/collections/page.tsx
@@ -1,26 +1,26 @@
-import { Button } from '@/components/ui';
-import {
-  LoginLink,
-  getKindeServerSession,
-} from '@kinde-oss/kinde-auth-nextjs/server';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+
+import { getUserCollections } from '@/db/handlers';
+import { UserCollection } from '@/types/collections';
+import { NoCollectionMessage, SignInAlert } from './components';
 
 export default async function CollectionsPage() {
-  const { isAuthenticated } = getKindeServerSession();
+  let userCollections: Array<UserCollection> = [];
+  const { isAuthenticated, getUser } = getKindeServerSession();
+  const [isUserSignedIn, user] = await Promise.all([
+    isAuthenticated(),
+    getUser(),
+  ]);
 
-  if (!(await isAuthenticated())) {
-    return (
-      <div className='flex w-full flex-1 flex-col items-center justify-center gap-y-2 px-[clamp(1rem,4dvw,3rem)] text-center'>
-        <h1 className='text-xl font-semibold'>
-          Please Sign In to View Your Photo Collections
-        </h1>
-        <p className='text-sm font-light'>
-          You need to be signed in to access your photo collections. Click the
-          button below to sign in.
-        </p>
-        <Button asChild className='mt-6'>
-          <LoginLink>Sign In</LoginLink>
-        </Button>
-      </div>
-    );
+  if (user) {
+    userCollections = await getUserCollections(user.id);
+  }
+
+  if (!isUserSignedIn) {
+    return <SignInAlert />;
+  }
+
+  if (userCollections.length === 0) {
+    return <NoCollectionMessage />;
   }
 }
