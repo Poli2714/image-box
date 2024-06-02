5724a887eff1cbd4bc2c8df09007d206394d2661
Elgun Mehdi <elgun88@gmail.com>
2024-06-02 07:36:14 -0600

    feat: add latest collections to photo slug page

--- src/app/(routes)/photo/[slug]/page.tsx
+++ src/app/(routes)/photo/[slug]/page.tsx
@@ -1,13 +1,14 @@
-import {
-  AddToCollectionBtn,
-  CollectionsToAddRemove,
-  DownloadLink,
-  PhotoAndTags,
-} from './components';
-import { AuthorImageAndName } from '@/components/ui';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+
+import { AddToCollectionBtn, DownloadLink, PhotoAndTags } from './components';
+import { AuthorImageAndName, LatestCollections } from '@/components/ui';
 
 import { formatDate } from '@/lib/formatDate/formatDate';
 import { getAPhoto } from '@/lib/getAPhoto';
+import { getLatestCollections } from '@/lib/getLatestCollections/getLatestCollections';
+import { getUserCollections } from '@/db/handlers';
+import { PhotoProvider } from '@/hooks/PhotoIdProvider';
+import { UserCollection } from '@/types/collections';
 
 type PhotoSlugPageProps = {
   params: {
@@ -16,30 +17,47 @@ type PhotoSlugPageProps = {
 };
 
 export default async function PhotoSlugPage({ params }: PhotoSlugPageProps) {
-  const photo = await getAPhoto(params.slug);
+  let userCollections: Array<UserCollection> = [];
+  const { getUser } = getKindeServerSession();
+  const [photo, user] = await Promise.all([getAPhoto(params.slug), getUser()]);
+  if (user) {
+    userCollections = await getUserCollections(user.id);
+  }
   const formattedDate = formatDate(photo.created_at);
+  const latestCollections = getLatestCollections(userCollections);
 
   return (
     <div className='grid grid-cols-1 gap-y-6 py-4 xl:grid-cols-2 xl:gap-x-6 xl:gap-y-0'>
       <PhotoAndTags keyword={params.slug} />
-      <div className='flex flex-col gap-y-4'>
-        <div className='flex items-center space-x-2'>
-          <AuthorImageAndName
-            portfolioUrl={photo.user.portfolio_url}
-            socialUrl={photo.user.links.html}
-            name={photo.user.name}
-            imageUrl={photo.user.profile_image.small}
-          />
+      <PhotoProvider
+        photoForCollection={{
+          altDescription: photo.alt_description,
+          id: photo.id,
+          slug: photo.slug,
+          thumb: photo.urls.thumb,
+        }}
+      >
+        <div className='flex flex-col gap-y-4'>
+          <div className='flex items-center space-x-2'>
+            <AuthorImageAndName
+              portfolioUrl={photo.user.portfolio_url}
+              socialUrl={photo.user.links.html}
+              name={photo.user.name}
+              imageUrl={photo.user.profile_image.small}
+            />
+          </div>
+          {formattedDate !== null ? (
+            <p className='text-xs font-light'>{`Published on ${formattedDate}`}</p>
+          ) : null}
+          <div className='mb-4 flex flex-col items-start space-y-4 xs:flex-row xs:space-x-4 xs:space-y-0'>
+            <AddToCollectionBtn />
+            <DownloadLink link={photo.links.download} />
+          </div>
+          {!!user ? (
+            <LatestCollections latestCollections={latestCollections} />
+          ) : null}
         </div>
-        {formattedDate !== null ? (
-          <p className='text-xs font-light'>{`Published on ${formattedDate}`}</p>
-        ) : null}
-        <div className='flex flex-col items-start space-y-4 xs:flex-row xs:space-x-4 xs:space-y-0'>
-          <AddToCollectionBtn />
-          <DownloadLink link={photo.links.download} />
-        </div>
-        <CollectionsToAddRemove />
-      </div>
+      </PhotoProvider>
     </div>
   );
 }
