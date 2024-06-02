5d08ef2b1cb67323b5abb00dd1a99804b3582442
Elgun Mehdi <elgun88@gmail.com>
2024-06-02 09:45:23 -0600

    fix: fix quote sign causing error on deployment

--- src/app/(routes)/collections/components/NoCollectionMessage/NoCollectionMessage.tsx
+++ src/app/(routes)/collections/components/NoCollectionMessage/NoCollectionMessage.tsx
@@ -5,8 +5,8 @@ function NoCollectionMessage() {
     <div className=' flex w-full flex-1 flex-col items-center justify-center gap-y-2 px-[clamp(1rem,4dvw,3rem)] text-center'>
       <h1 className='text-xl font-semibold'>No Collections Yet</h1>
       <p className='w-3/4 text-sm font-light text-secondary-foreground'>
-        You haven't created any collections yet. Start exploring and add your
-        favorite photos to your collections.
+        You haven&apos;t created any collections yet. Start exploring and add
+        your favorite photos to your collections.
       </p>
       <div className='mt-4 w-3/4 max-w-lg'>
         <SearchForm />
