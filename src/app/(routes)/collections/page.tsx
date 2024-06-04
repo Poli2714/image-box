d3e223cdff90f42d440cbdb5b8d918e3a2e7bb5c
Elgun Mehdi <elgun88@gmail.com>
2024-06-04 06:00:28 -0600

    feat: create collections main page

--- src/app/(routes)/collections/page.tsx
+++ src/app/(routes)/collections/page.tsx
@@ -2,7 +2,13 @@ import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 
 import { getUserCollections } from '@/db/handlers';
 import { UserCollection } from '@/types/collections';
-import { NoCollectionMessage, SignInAlert } from './components';
+import {
+  CollectionCard,
+  CollectionsHeader,
+  NoCollectionMessage,
+  SignInAlert,
+} from './components';
+import Link from 'next/link';
 
 export default async function CollectionsPage() {
   let userCollections: Array<UserCollection> = [];
@@ -23,4 +29,19 @@ export default async function CollectionsPage() {
   if (userCollections.length === 0) {
     return <NoCollectionMessage />;
   }
+
+  return (
+    <main className='space-y-12 px-[clamp(1rem,4dvw,3rem)] py-10 2xl:container'>
+      <CollectionsHeader />
+      <div>
+        <ul className='grid grid-cols-[repeat(auto-fit,minmax(clamp(15rem,70dvw,25rem),1fr))] gap-4'>
+          {userCollections.map((userCollection) => (
+            <li className='flex justify-center' key={userCollection.id}>
+              <CollectionCard collection={userCollection} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </main>
+  );
 }
