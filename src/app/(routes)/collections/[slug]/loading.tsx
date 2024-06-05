f314bd4ff09ec7ad534be86b643299243e01b5f2
Elgun Mehdi <elgun88@gmail.com>
2024-06-05 06:36:42 -0600

    feat: create loading for several pages

---
+++ src/app/(routes)/collections/[slug]/loading.tsx
@@ -0,0 +1,13 @@
+import { Loader2Icon } from 'lucide-react';
+
+export default function CollectionSlugLoadingPage() {
+  return (
+    <div className='mt-20 flex items-center justify-center'>
+      <Loader2Icon
+        strokeWidth={1.5}
+        size={64}
+        className='animate-spin text-secondary'
+      />
+    </div>
+  );
+}
--- src/app/(routes)/search/[slug]/loading.tsx
+++ src/app/(routes)/search/[slug]/loading.tsx
@@ -1,3 +1,4 @@
+import { Loader2Icon } from 'lucide-react';
 import { Skeleton } from '@/components/ui';
 
 import { createArray } from '@/lib/createArray/createArray';
@@ -6,12 +7,19 @@ export default function SearchLoadingPage() {
   const arr = createArray(21);
 
   return (
-    <ul className='grid grid-cols-[repeat(auto-fit,minmax(clamp(20rem,50dvw,25rem),1fr))] gap-6'>
-      {arr.map((_, i) => (
-        <li key={i}>
-          <Skeleton className='skeletonMd:h-[clamp(16.7rem,30dvw,25.5rem)] skeletonLg:h-[clamp(16.7rem,20dvw,19rem)] h-[clamp(12.75rem,50dvw,32rem)] w-full rounded-md' />
-        </li>
-      ))}
-    </ul>
+    // <ul className='grid grid-cols-[repeat(auto-fit,minmax(clamp(20rem,50dvw,25rem),1fr))] gap-6'>
+    //   {arr.map((_, i) => (
+    //     <li key={i}>
+    //       <Skeleton className='skeletonMd:h-[clamp(16.7rem,30dvw,25.5rem)] skeletonLg:h-[clamp(16.7rem,20dvw,19rem)] h-[clamp(12.75rem,50dvw,32rem)] w-full rounded-md' />
+    //     </li>
+    //   ))}
+    // </ul>
+    <div className='mt-10 flex items-center justify-center'>
+      <Loader2Icon
+        strokeWidth={1.5}
+        size={64}
+        className='animate-spin text-secondary'
+      />
+    </div>
   );
 }
