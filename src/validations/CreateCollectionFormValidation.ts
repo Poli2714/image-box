85242cd858402872bcf7117b076a17e43deff178
Elgun Mehdi <elgun88@gmail.com>
2024-06-09 04:47:54 -0600

    feat: add max length requirement to CreateCollectionForm

--- src/validations/CreateCollectionFormValidation.ts
+++ src/validations/CreateCollectionFormValidation.ts
@@ -1,10 +1,17 @@
 import { z } from 'zod';
 
 export const createCollectionFormSchema = z.object({
-  name: z.string().trim().min(1, {
-    message:
-      'Please enter a name with at least one character to create a collection.',
-  }),
+  name: z
+    .string()
+    .trim()
+    .min(1, {
+      message:
+        'Please enter a name with at least one character to start your collection.',
+    })
+    .max(50, {
+      message:
+        'The collection name cannot exceed 50 characters. Please enter a shorter name.',
+    }),
 });
 
 export type CreateCollectionFormSchemaProps = z.infer<
