949d392999444cc15d9987c88962b28a9de4275f
Elgun Mehdi <elgun88@gmail.com>
2024-06-08 22:07:00 -0600

    feat: add CollectionName and EditCollectionBtn to CollectionSlugHeader

--- src/app/(routes)/collections/[slug]/components/CollectionSlugHeader/CollectionSlugHeader.test.tsx
+++ src/app/(routes)/collections/[slug]/components/CollectionSlugHeader/CollectionSlugHeader.test.tsx
@@ -1,17 +1,21 @@
-import { expect, test } from 'vitest';
+import { expect, test, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
 import CollectionSlugHeader from './CollectionSlugHeader';
 
+vi.mock('./components', () => ({
+  CollectionName: () => <h1>Mock Collection name</h1>,
+  EditCollectionBtn: () => <button>Mock Edit</button>,
+}));
+
 test('renders CollectionSlugHeader', () => {
   render(
-    <CollectionSlugHeader collectionName='Test name' createdAt={new Date()} />
+    <CollectionSlugHeader collectionId='sbhjdvbsdv' createdAt={new Date()} />
   );
 
   expect(screen.getByRole('banner')).toBeInTheDocument();
-  expect(screen.getByRole('heading', { level: 1 }).textContent).toMatch(
-    /^test name$/i
-  );
-  expect(screen.getByRole('button', { name: 'Edit' })).toBeInTheDocument();
+  expect(screen.getByRole('heading', { level: 1 })).toBeInTheDocument();
+  expect(screen.getByRole('button')).toBeInTheDocument();
   expect(screen.getByRole('paragraph')).toBeInTheDocument();
+  expect(screen.getByRole('paragraph').textContent).toMatch(/^created on.*$/i);
 });
--- src/app/(routes)/collections/[slug]/components/CollectionSlugHeader/CollectionSlugHeader.tsx
+++ src/app/(routes)/collections/[slug]/components/CollectionSlugHeader/CollectionSlugHeader.tsx
@@ -1,29 +1,22 @@
-import { Button } from '@/components/ui';
+import { CollectionName, EditCollectionBtn } from './components';
 
 import { formatDate } from '@/lib/formatDate/formatDate';
 
 type CollectionSlugHeaderProps = {
-  collectionName: string;
+  collectionId: string;
   createdAt: Date;
 };
 
 function CollectionSlugHeader({
-  collectionName,
+  collectionId,
   createdAt,
 }: CollectionSlugHeaderProps) {
   const formattedDate = formatDate(createdAt);
 
   return (
     <header className='grid grid-flow-col grid-cols-[1fr,min-content]'>
-      <h1 className='bg-[linear-gradient(to_left,_#F2C593,_#8A3282)] bg-clip-text text-3xl text-transparent lg:text-4xl'>
-        {collectionName}
-      </h1>
-      <Button
-        className='col-start-2 row-span-2 place-self-center'
-        variant='outline'
-      >
-        Edit
-      </Button>
+      <CollectionName />
+      <EditCollectionBtn collectionId={collectionId} />
       <p className='mt-1 text-sm font-light lg:mt-2'>
         Created on {formattedDate}
       </p>
