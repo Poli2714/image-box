362a9267120d763bd4745557dbac8af4c62f747b
Elgun Mehdi <elgun88@gmail.com>
2024-06-10 19:32:01 -0600

    fix: fix an error when deleting an empty collection

--- src/actions/deleteCollection.ts
+++ src/actions/deleteCollection.ts
@@ -11,63 +11,74 @@ export const deleteCollection = async (collectionId: string) => {
   const { getUser } = getKindeServerSession();
 
   try {
-    const user = await getUser();
+    const [user, photosInDeletedCollection] = await Promise.all([
+      getUser(),
+      db.query.collectionsToPhotos.findMany({
+        where: (collectionsToPhotos, { eq }) =>
+          eq(collectionsToPhotos.collectionId, collectionId),
+      }),
+    ]);
     if (!user) {
       throw new Error('User not found');
     }
-    const [deletedCollectionPhotos, allUserCollectionPhotos] =
-      await Promise.all([
-        db
-          .delete(collectionsToPhotos)
-          .where(eq(collectionsToPhotos.collectionId, collectionId))
-          .returning({
-            id: collectionsToPhotos.photoId,
-          }),
-        db.query.collections
-          .findMany({
-            columns: {
-              id: true,
-            },
-            where: (collections, { and, eq, ne }) =>
-              and(
-                ne(collections.id, collectionId),
-                eq(collections.userId, user.id)
-              ),
-          })
-          .then((userCollectionIds) =>
-            db.query.collectionsToPhotos.findMany({
+
+    if (photosInDeletedCollection.length === 0) {
+      await db.delete(collections).where(eq(collections.id, collectionId));
+    } else {
+      const [deletedCollectionPhotos, allUserCollectionPhotos] =
+        await Promise.all([
+          db
+            .delete(collectionsToPhotos)
+            .where(eq(collectionsToPhotos.collectionId, collectionId))
+            .returning({
+              id: collectionsToPhotos.photoId,
+            }),
+          db.query.collections
+            .findMany({
               columns: {
-                photoId: true,
+                id: true,
               },
-              where: (collectionsToPhotos, { inArray }) =>
-                inArray(
-                  collectionsToPhotos.collectionId,
-                  userCollectionIds.map(
-                    (userCollectionId) => userCollectionId.id
-                  )
+              where: (collections, { and, eq, ne }) =>
+                and(
+                  ne(collections.id, collectionId),
+                  eq(collections.userId, user.id)
                 ),
             })
-          ),
-      ]);
-    await Promise.all([
-      db.delete(photos).where(
-        and(
-          inArray(
-            photos.id,
-            deletedCollectionPhotos.map(
-              (deletedCollectionPhoto) => deletedCollectionPhoto.id
-            )
-          ),
-          notInArray(
-            photos.id,
-            allUserCollectionPhotos.map(
-              (userCollection) => userCollection.photoId
+            .then((userCollectionIds) =>
+              db.query.collectionsToPhotos.findMany({
+                columns: {
+                  photoId: true,
+                },
+                where: (collectionsToPhotos, { inArray }) =>
+                  inArray(
+                    collectionsToPhotos.collectionId,
+                    userCollectionIds.map(
+                      (userCollectionId) => userCollectionId.id
+                    )
+                  ),
+              })
+            ),
+        ]);
+      await Promise.all([
+        db.delete(photos).where(
+          and(
+            inArray(
+              photos.id,
+              deletedCollectionPhotos.map(
+                (deletedCollectionPhoto) => deletedCollectionPhoto.id
+              )
+            ),
+            notInArray(
+              photos.id,
+              allUserCollectionPhotos.map(
+                (userCollection) => userCollection.photoId
+              )
             )
           )
-        )
-      ),
-      db.delete(collections).where(eq(collections.id, collectionId)),
-    ]);
+        ),
+        db.delete(collections).where(eq(collections.id, collectionId)),
+      ]);
+    }
   } catch (error) {
     if (error instanceof Error) {
       throw new Error(error.message);
